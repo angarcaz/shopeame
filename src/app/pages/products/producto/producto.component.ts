724c26cdd556431721502eafe1829aeafba1f749
angarcaz93@gmail.com <angarcaz93@gmail.com>
2023-02-19 21:13:29 +0100

    componente producto

--- src/app/pages/products/producto/producto.component.ts
+++ src/app/pages/products/producto/producto.component.ts
@@ -1,5 +1,7 @@
+import { productInterface } from 'src/app/models/item-interface';
 import { ProductoServicioService } from './../../../servicios/producto-servicio.service';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-producto',
@@ -7,17 +9,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./producto.component.scss']
 })
 export class ProductoComponent {
-  productList : any [] = [];
+  // productList : any [] = [];
+  id!: number;
+  product!: productInterface
   
-  constructor(private product:ProductoServicioService){}
+  constructor(private productoServicio: ProductoServicioService, private activateRoute: ActivatedRoute, private router:Router){}
 
   ngOnInit(): void {
-    this.product.getProducts().subscribe((data:any)=>{
-      console.log(data)
+
+    //Lee la ruta que ponemnos en el navegador y la guarda en la variable id
+    this.activateRoute.paramMap.subscribe(params => {
+      this.id = Number(params.get("id"))
+    })
+
+    //LLamamos al servicio que tiene la funcion getItem, que hace una petición individual 
+    this.productoServicio.getItem(this.id).subscribe((data:any)=> {
+      this.product = data;
+    })
+
+    this.productoServicio.postItem(this.id).subscribe((data:any)=>{
+      this.product = data
+    })
+  }
+  //Función para eliminar el elemento. Redirige a productos. 
+  removeItem() {
+    this.productoServicio.removeItem(this.id).subscribe((data:any) => {
+      this.product = data
+      this.router.navigate(["/productos"])
     })
   }
 
+
 }
+
+
+
+
+
+
+
+
+
+
+//PRUEBAS BUSCADOR
+
+
+//   ngOnInit(): void {
+//     this.product.getProducts().subscribe((data:any)=>{
+//       console.log(data)
+//     })
+//   }
+
+// }
   
 
   //productList: string[];
